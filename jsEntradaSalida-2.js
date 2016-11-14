eacf5362c6bc983ffaa1191e95a58eb93e6c1bd4
ariel0080 <ariel0080@gmail.com>
2016-11-14 11:43:26 -0300

    Clase0114112016

---
+++ jsEntradaSalida-2.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+var nombre;
+var apellido = "Brino";
+nombre = "Ariel";
+alert("Mi nombre es: " + nombre + " y su apellido es " + apellido);
+
+nombre = prompt("Ingrese su nombre","nn");
+
+alert(nombre)
+}
+
